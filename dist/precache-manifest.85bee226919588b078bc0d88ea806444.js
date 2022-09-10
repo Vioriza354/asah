03b83ff2b1b58bc6077357f165b05950151ba26a
Vioriza Qiyaski <viorizajkm18@gmail.com>
2022-09-11 04:01:56 +0700

    initila commit

---
+++ dist/precache-manifest.85bee226919588b078bc0d88ea806444.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a1e682e5e0526bdc67f",
+    "url": "/css/app.bdd88112.css"
+  },
+  {
+    "revision": "4a1e682e5e0526bdc67f",
+    "url": "/js/app.24ba1953.js"
+  },
+  {
+    "revision": "216cbbd27f1065cb5b98",
+    "url": "/css/chunk-4e0b8328.bc91fb24.css"
+  },
+  {
+    "revision": "216cbbd27f1065cb5b98",
+    "url": "/js/chunk-4e0b8328.953c32ff.js"
+  },
+  {
+    "revision": "5fffce0aa8462264b116",
+    "url": "/js/chunk-52e4de86.f4ed7d66.js"
+  },
+  {
+    "revision": "c641fb9beffa8db2ed29",
+    "url": "/js/chunk-vendors.95feff96.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "21d37d2dcd872b81277db11c2061f140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "9b5f4f0f73caa60f388ba05008cf7ccd",
+    "url": "/img/brand/gblack_asah.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "450dff3f0a2b767aeab204b370517d9f",
+    "url": "/img/brand/green_asah.png"
+  },
+  {
+    "revision": "03e5c1f7164334ddf11deb28ada9af31",
+    "url": "/img/brand/gwhite_asah.png"
+  },
+  {
+    "revision": "a173b9a5ee884f47c98d0cbb6b256cbb",
+    "url": "/img/brand/icon_asah.png"
+  },
+  {
+    "revision": "bd4fded3dd1256c0ebda3cda7d3ea899",
+    "url": "/img/brand/white_asah.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fa2d45006d43cdc1120d41606827ea3b",
+    "url": "/img/theme/ikan2.png"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "5dc3a03be14b7fe57461dfad9e2cee44",
+    "url": "/img/theme/5asli.png"
+  },
+  {
+    "revision": "416de2948760460506581686a33e9f1b",
+    "url": "/img/theme/4asli.png"
+  },
+  {
+    "revision": "a0787d184c67ae8ea2d6ecacf6077e25",
+    "url": "/img/theme/3asli.png"
+  },
+  {
+    "revision": "2f0f6ae517ebfb0e43992dd0df93aec1",
+    "url": "/img/theme/6asli.png"
+  },
+  {
+    "revision": "7258db943126b4eb0bf2f6127d49dfde",
+    "url": "/img/theme/8asli.png"
+  },
+  {
+    "revision": "09ef3d4f8aeef358943e1406deac6685",
+    "url": "/img/theme/9asli.png"
+  },
+  {
+    "revision": "22f7b90716e9ec0a399e9d722a3f5727",
+    "url": "/img/theme/7asli.png"
+  },
+  {
+    "revision": "32e1940b2cb827b66f97180c316ad4b5",
+    "url": "/img/theme/close-up-hand-writing-notebook-top-view.jpg"
+  },
+  {
+    "revision": "f31d8b1c55c6d4a5d83dc3a828c3717d",
+    "url": "/img/theme/anj1.jpg"
+  },
+  {
+    "revision": "f31d8b1c55c6d4a5d83dc3a828c3717d",
+    "url": "/img/theme/anj7.jpg"
+  },
+  {
+    "revision": "febb7ff529d1d2086353e56b668282fb",
+    "url": "/img/aset/carosel2.jpg"
+  },
+  {
+    "revision": "d2441724bd0cd2e9446e0e19b77dcaa9",
+    "url": "/img/theme/anj2.jpg"
+  },
+  {
+    "revision": "2a64732c3a9c491f1e283bbf33334bc8",
+    "url": "/img/theme/anj6.jpg"
+  },
+  {
+    "revision": "9e828fc43d16a0dd33906b934d5add3f",
+    "url": "/img/theme/anj5.jpg"
+  },
+  {
+    "revision": "2b7bbf1ab1c3344a33d299814ab907aa",
+    "url": "/img/theme/iklan1.png"
+  },
+  {
+    "revision": "3019b126654dcec488360686c8dd40ef",
+    "url": "/img/aset/carosel1.jpg"
+  },
+  {
+    "revision": "3a86626bca6b59919cc7d44568cb57cc",
+    "url": "/img/theme/anj8.jpg"
+  },
+  {
+    "revision": "46448a8f60d5b888eaf00b43ebe9c66e",
+    "url": "/img/theme/anj9.jpg"
+  },
+  {
+    "revision": "5ea5b9b491d43204998c2cba9fe64b4c",
+    "url": "/img/theme/anj3.jpg"
+  },
+  {
+    "revision": "ba0544708ee72bebd0672ca22116acd2",
+    "url": "/img/theme/anj10.jpg"
+  },
+  {
+    "revision": "0524c081194a1a56d934083cfc22d4ab",
+    "url": "/img/theme/iklan2.png"
+  },
+  {
+    "revision": "8d6b4822654ffeca62fcad39a3799106",
+    "url": "/img/theme/anj4.jpg"
+  },
+  {
+    "revision": "fba1d8443a3d21965517dd3410b1bd09",
+    "url": "/img/theme/gedung.png"
+  },
+  {
+    "revision": "7ec60bbb1e9e3415ac49e415efd5b28b",
+    "url": "/img/theme/asahimg.png"
+  }
+];
