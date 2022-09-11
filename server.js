7481d43bbb6315a21b02da82bcf00a1cd9d8ef89
rizkyaziz <rizkyaziz2002@gmail.com>
2022-09-11 18:00:56 +0700

    build

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const serveStatic = require('server-static')
+const serveStatic = require('serve-static')
 const path = require('path')
 
 
 const app = express()
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8000
 app.listen(port)
